74aa65620fc1cb519ec17c3102b13416b6faa6b9
dannyfrelink <danny-frelink@hotmail.com>
2023-04-26 18:51:35 +0200

    feat: create home screen

---
+++ src/components/CloudButton.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import cloud from "../svg/cloud.svg";
+
+interface CloudButtonProps {
+    className: string;
+    href: string;
+    iconClass: string;
+    icon: any;
+    iconAlt: string;
+    textClass: string;
+    text: string;
+
+}
+
+const CloudButton = ({
+    className,
+    href,
+    iconClass,
+    icon,
+    iconAlt,
+    textClass,
+    text
+}: CloudButtonProps) =>  (
+    <a href={href} className={`absolute ${className}`}>
+    <Image
+        className={`absolute top-[50px] z-[1] scale-110 ${iconClass}`}
+        src={icon}
+        alt={`${iconAlt} icoon`}
+    />
+
+    <p className={`absolute top-6 text-center z-[1] font-semibold text-primary-dark text-[17px] ${textClass}`}>
+        {text.split(" ")[0]}
+        <br/>
+        {text.split(" ")[1]}
+    </p>
+
+    <Image
+        className='scale-110'
+        src={cloud}
+        alt="Illustatie wolk als button"
+    />
+    </a>
+);
+  
+export default CloudButton;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-light": "#F8F8F8",
+        "primary-dark": "#1D4045",
+        "secondary-light": "#58C3D1",
+        "secondary-dark": "#387C85",
       },
     },
   },
   plugins: [],
-}
+};
