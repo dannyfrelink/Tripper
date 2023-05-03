5ff2ea49636cd37dd95d91a41d9a619eb8466e3f
dannyfrelink <danny-frelink@hotmail.com>
2023-05-03 15:01:44 +0200

    update: clear up unused code

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,23 +8,21 @@ interface MapProps {
 
 const Map = ({
 
-}: MapProps) => {
-    return (
-        <ReactMap
-            initialViewState={{
-                // Lat & Long of Bali
-                latitude: -8.45,
-                longitude: 115.05,
-                zoom: 7.45
-            }}
-            style={{
-                width: '100%',
-                height: '208px'
-            }}
-            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
-            mapStyle="mapbox://styles/mapbox/streets-v11"
-        ></ReactMap>
-    )
-}
+}: MapProps) =>  (
+    <ReactMap
+        initialViewState={{
+            // Lat & Long of Bali
+            latitude: -8.45,
+            longitude: 115.05,
+            zoom: 7.45
+        }}
+        style={{
+            width: '100%',
+            height: '208px'
+        }}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+    ></ReactMap>
+);
 
 export default Map;
