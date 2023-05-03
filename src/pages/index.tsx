457f90f0467e7674222ac84c5df55e8c324572ab
dannyfrelink <danny-frelink@hotmail.com>
2023-05-03 11:16:47 +0200

    update: make page design responsive for mobiles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,43 +10,45 @@ export default function Home() {
     <main
       className='bg-secondary-light h-screen'
     >
-      <header className='mx-auto pt-24 w-44'>
-        <Image
-          className='m-auto scale-110'
-          src={logo}
-          alt="Tripper logo"
+      <div className='h-[570px] w-screen absolute bottom-0'>
+        <header className='mx-auto w-44'>
+          <Image
+            className='m-auto scale-110'
+            src={logo}
+            alt="Tripper logo"
+          />
+
+          <p className='text-primary-dark text-xl text-right mt-1'>
+            Bali editie
+          </p>
+        </header>
+
+        <CloudButton
+          className='bottom-[350px] left-10'
+          href='activities'
+          iconClass='left-2'
+          icon={airplaneIcon}
+          iconAlt='Vliegtuig'
+          textClass='left-[35px]'
+          text='Reisroute samenstellen'
         />
 
-        <p className='text-primary-dark text-xl text-right mt-1'>
-          Bali editie
-        </p>
-      </header>
+        <CloudButton
+          className='bottom-64 right-10'
+          href='info'
+          iconClass='left-2.5'
+          icon={infoIcon}
+          iconAlt='Informatie'
+          textClass='left-11'
+          text='Belangrijke informatie'
+        />
 
-      <CloudButton
-        className='top-56 left-10'
-        href='activities'
-        iconClass='left-2'
-        icon={airplaneIcon}
-        iconAlt='Vliegtuig'
-        textClass='left-[35px]'
-        text='Reisroute samenstellen'
-      />
-
-      <CloudButton
-        className='top-[304px] right-10'
-        href='info'
-        iconClass='left-2.5'
-        icon={infoIcon}
-        iconAlt='Informatie'
-        textClass='left-11'
-        text='Belangrijke informatie'
-      />
-
-      <Image
-        className='w-screen absolute bottom-0'
-        src={illustration}
-        alt="Illustatie zonnig strand"
-      />
+        <Image
+          className='w-screen absolute bottom-0'
+          src={illustration}
+          alt="Illustatie zonnig strand"
+        />
+      </div>
     </main>
   )
 }
