2a5a391889029e284985eeaf828508fb51ab5e5a
dannyfrelink <danny-frelink@hotmail.com>
2023-05-08 21:07:51 +0200

    feat: create seperate component voor ActivityDetailsContent

---
+++ src/components/ActivityDetailsContent.tsx
@@ -0,0 +1,33 @@
+interface ActivityDetailsContentProps {
+    activity: {
+        id: number,
+        name: string,
+        image: {
+            src: string,
+            alt: string
+        },
+        tourism: number,
+        price: number,
+        tags: string,
+        location: string,
+        href: string,
+        details: string
+    };
+}
+
+const ActivityDetailsContent = ({
+    activity
+}: ActivityDetailsContentProps) => {
+
+    return (
+        <div className='text-primary-dark text-[15px] w-11/12 mx-auto px-3.5 [&>*:not(:first-of-type)]:mt-2.5 my-6'>
+        {activity.details.split('//').map((paragraph: string) => 
+            <p>{paragraph}</p>
+        )}
+
+        <a className='block w-fit text-[15px] border-[1px] border-primary-dark rounded-md py-0.5 px-1.5 mt-4' href="/activities">Verder zoeken</a>
+    </div>
+    );
+}
+  
+export default ActivityDetailsContent;
--- src/pages/activities/[activity].tsx
+++ src/pages/activities/[activity].tsx
@@ -43,13 +43,9 @@ export default function ActivitiesDetail({activity}: any) {
                 favouriteActivity={favouriteActivity}
             />
 
-            <div className='text-primary-dark text-[15px] w-11/12 mx-auto px-3.5 [&>*:not(:first-of-type)]:mt-2.5 my-6'>
-                {activity.details.split('//').map((paragraph: string) => 
-                    <p>{paragraph}</p>
-                )}
-
-                <a className='block w-fit text-[15px] border-[1px] border-primary-dark rounded-md py-0.5 px-1.5 mt-4' href="/activities">Verder zoeken</a>
-            </div>
+        <ActivityDetailsContent
+            activity={activity}
+        />
         </main>
     )
 }
@@ -58,6 +54,7 @@ export default function ActivitiesDetail({activity}: any) {
 import fsPromises from 'fs/promises';
 import path from 'path';
 import { useEffect, useState } from 'react';
+import ActivityDetailsContent from '@/components/ActivityDetailsContent';
 
 export async function getStaticPaths() {
     const filePath = path.join(process.cwd(), 'activities.json');
