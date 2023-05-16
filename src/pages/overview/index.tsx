895a2bc76bfee0dc9915e05d666db26b104c0cf6
dannyfrelink <danny-frelink@hotmail.com>
2023-05-16 13:46:49 +0200

    update: change name of activity

--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -88,7 +88,7 @@ export default function Overview({activities}: any) {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            prompt: `Maak een reisroute voor ${daysInTextPerLocation[location]} dagen met enkel deze activiteiten in lopende tekst: ${activities.join(", ")}. Er mogen geen andere activiteiten bijkomen.`
+            prompt: `Maak een reisroute voor ${daysInTextPerLocation[location]} dagen met enkel deze activiteiten in lopende tekst: ${activities.join(", ")}. Er mogen geen andere activiteiten bijkomen. Splits de dagen op in aparte alinea's.`
           })
         }).then(res => res.json());
 
