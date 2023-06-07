5dcc7177d0f5b863d1ed67959edbdf36f42d271b
dannyfrelink <danny-frelink@hotmail.com>
2023-06-07 13:29:34 +0200

    feat: make markers for activities clickable to show more info

--- src/components/MapLocation.tsx
+++ src/components/MapLocation.tsx
@@ -1,6 +1,7 @@
 import ReactMap from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapLocationMarker from './MapLocationMarker';
+import { useState } from 'react';
 
 interface Activity {
     id: number,
@@ -26,68 +27,78 @@ interface MapLocationProps {
 const MapLocation = ({
     activities,
     location
-}: MapLocationProps) => (
-    <ReactMap
-        initialViewState={
-            location === 'nusa' ?
-                {
-                    // Lat, Long & Zoom of Nusa
-                    latitude: -8.738,
-                    longitude: 115.525,
-                    zoom: 9.65
-                } :
-            location === 'canggu' ?
-                {
-                    // Lat, Long & Zoom of Canggu
-                    latitude: -8.643,
-                    longitude: 115.117,
-                    zoom: 11.3
-                } :
-            location === 'uluwatu' ?
-                {
-                    // Lat, Long & Zoom of Uluwatu
-                    latitude: -8.812,
-                    longitude: 115.12,
-                    zoom: 10.9
-                } :
-            location === 'ubud' ?
-                {
-                    // Lat, Long & Zoom of Ubud
-                    latitude: -8.45,
-                    longitude: 115.25,
-                    zoom: 9.5
-                } :
-                {
-                    // Lat, Long & Zoom of Amed
-                    latitude: -8.38,
-                    longitude: 115.623,
-                    zoom: 10.35
-                }
-        }
-        style={{
-            width: '100%',
-            height: '208px',
-            boxShadow: '0px 3px 15px rgba(0, 0, 0, 0.2)'
-        }}
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-    >
-        {
-            activities.map((activity, index) => {
-                const coordinates = activity.coordinates;
-                const latLong = coordinates.split(", ");
+}: MapLocationProps) => {
+    const [openMarker, setOpenMarker] = useState('');
 
-                return (
-                    <MapLocationMarker
-                        key={index}
-                        latitude={Number(latLong[0])}
-                        longitude={Number(latLong[1])}
-                        icon={activity.icon}
-                    />
+    const onClick = (e:any) => {
+        let element = e.target.tagName.toLowerCase();
+        let target;
+        if(element === 'img') {
+            target = e.target.parentNode
+        } else {target = e.target}
+
+        setOpenMarker(target.id);
+    }
+
+    return (
+        <ReactMap
+            initialViewState={
+                location === 'nusa' ?
+                    {
+                        // Lat, Long & Zoom of Nusa
+                        latitude: -8.738,
+                        longitude: 115.525,
+                        zoom: 9.65
+                    } :
+                location === 'canggu' ?
+                    {
+                        // Lat, Long & Zoom of Canggu
+                        latitude: -8.643,
+                        longitude: 115.117,
+                        zoom: 11.3
+                    } :
+                location === 'uluwatu' ?
+                    {
+                        // Lat, Long & Zoom of Uluwatu
+                        latitude: -8.812,
+                        longitude: 115.12,
+                        zoom: 10.9
+                    } :
+                location === 'ubud' ?
+                    {
+                        // Lat, Long & Zoom of Ubud
+                        latitude: -8.45,
+                        longitude: 115.25,
+                        zoom: 9.5
+                    } :
+                    {
+                        // Lat, Long & Zoom of Amed
+                        latitude: -8.38,
+                        longitude: 115.623,
+                        zoom: 10.35
+                    }
+            }
+            style={{
+                width: '100%',
+                height: '208px',
+                boxShadow: '0px 3px 15px rgba(0, 0, 0, 0.2)'
+            }}
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+        >
+            {
+                activities.map((activity, index) => (
+                        <MapLocationMarker
+                            key={index}
+                            activity={activity}
+                            onClick={onClick}
+                            openMarker={openMarker}
+                        />
+                    )
                 )
-            })
-        }
-    </ReactMap>
-);
+            }
+        </ReactMap>
+    );
+}
 
 export default MapLocation;
--- src/components/MapLocationMarker.tsx
+++ src/components/MapLocationMarker.tsx
@@ -1,30 +1,114 @@
 import { Marker } from 'react-map-gl';
 import Image from 'next/image';
+import { useState } from 'react';
+import tourism from '../svg/tourism.svg';
+import price from '../svg/price.svg';
+
+interface Activity {
+    id: number,
+    name: string,
+    image: {
+        src: string,
+        alt: string
+    },
+    tourism: number,
+    price: number,
+    tags: string,
+    icon: string,
+    location: string,
+    coordinates: string,
+    details: string
+};
 
 interface MapLocationMarkerProps {
-    latitude: number;
-    longitude: number;
-    icon: string;
+    activity: Activity;
+    onClick: (e:any) => void;
+    openMarker: string;
 }
 
 const MapLocationMarker = ({
-    latitude,
-    longitude,
-    icon
-}: MapLocationMarkerProps) => (
-    <Marker
-        latitude={latitude}
-        longitude={longitude}
-    >
-        <div className='bg-secondary-dark h-6 w-6 p-1 shadow-subtle rounded-2xl'>
-            <Image
-                src={`/assets/activity-icons/icon-${icon}.png`}
-                alt={`${icon} icon`}
-                width={500}
-                height={500}
-            />
-        </div>
-    </Marker>
-);
+    activity,
+    onClick,
+    openMarker
+}: MapLocationMarkerProps) => {
+    const coordinates = activity.coordinates;
+    const latLong = coordinates.split(", ");
+    const latitude = Number(latLong[0]);
+    const longitude = Number(latLong[1]);
+    const icon = activity.icon;
+    const iconCount = ["", "", ""];
+
+    return (
+        <Marker
+            latitude={latitude}
+            longitude={longitude}
+            style={openMarker === activity.id.toString() ? {
+                zIndex: '50'
+            } : {
+                zIndex: '0'
+            }}
+        >
+            {
+                openMarker !== activity.id.toString() ?
+                    <div
+                        id={activity.id.toString()}
+                        className='bg-secondary-dark h-6 w-6 p-1 shadow-subtle rounded-2xl'
+                        onClick={onClick}
+                    >
+                        <Image
+                            src={`/assets/activity-icons/icon-${icon}.png`}
+                            alt={`${icon} icon`}
+                            width={500}
+                            height={500}
+                        /> 
+                    </div> :
+                    <div
+                        id={activity.id.toString()}
+                        className='flex bg-secondary-dark text-primary-light text-sm py-1.5 px-2 rounded-lg'
+                    >
+                        <Image
+                            src={activity.image.src}
+                            alt={activity.image.alt}
+                            width={500}
+                            height={500}
+                            className='w-12 min-h-[64px] object-cover object-center mr-1.5'
+                        />
+
+                        <div className='mt-0.5'>
+                            <h2>
+                                {activity.name}
+                            </h2>
+
+                            <div className='flex justify-between w-10 mt-1'>
+                                {
+                                    iconCount.map((c, index) =>
+                                        <Image
+                                            key={index}
+                                            className={`scale-110 ${index < activity.tourism ? '' : 'opacity-30'}`}
+                                            src={tourism}
+                                            alt="Poppetje icoon"
+                                        />
+                                    )
+                                }
+                            </div>
+
+                            <div className='flex justify-between w-9 ml-0.5 mt-2'>
+                                {
+                                iconCount.map((c, index) =>
+                                        <Image
+                                            key={index}
+                                            className={`scale-110 ${index < activity.price ? '' : 'opacity-30'}`}
+                                            src={price}
+                                            alt="Euro icoon"
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </div>
+            }
+        </Marker>
+    );
+}
 
 export default MapLocationMarker;
